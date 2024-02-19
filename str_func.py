3397076fcafeb3284a25556b66840db2fb763e16
Anna Soboleva <asoboleva.avs@gmail.com>
2024-02-19 22:58:52 +0300

    added new function str_to_title_words() in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def str_func(string_value):
+    """Принимает на вход строку
+    и возвращает ее со всеми заглавными буквами """
     return string_value.upper()
+
+def str_to_title_words(string_value):
+    """делает заглавными первые буквы каждого слова в строке,
+     поступившей на вход функции"""
+    return string_value.title()
